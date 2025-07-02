18b48a177cafe6a951c14741aae1fa90275cba46
maruyaofficial <jasminemontirola@gmail.com>
2025-07-03 00:05:47 +0800

    Update proxy.php

--- public/proxy.php
+++ public/proxy.php
@@ -1,28 +1,7 @@
+
 <?php
 // Target URL
 $targetUrl = 'https://home.nathcreqtives.com/';
-
-// Use cURL to fetch the external site
-$ch = curl_init($targetUrl);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-curl_setopt($ch, CURLOPT_HEADER, false);
-
-$response = curl_exec($ch);
-$contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-curl_close($ch);
-
-// Send correct headers
-header("Content-Type: $contentType");
-header("X-Frame-Options: ALLOWALL"); // Optional override
-header("Access-Control-Allow-Origin: *"); // Optional if needed
-
-// Output the response
-echo $response;
-
-
-$targetUrl = 'https://proxy.nathcreqtives.com/';
 
 // Use cURL to fetch the external site
 $ch = curl_init($targetUrl);
